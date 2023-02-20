765318cf29060a960b44937ff70f644725046707
Gokul Gandhi <gokulgandhi97@gmail.com>
2023-02-20 22:37:55 +0530

    Simple Calculator using react js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Calculator from "./components/Calculator";
+import "./components/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator /> {/*Getting the calculator component by importing */}
+    </div>
+  );
+}
+
+export default App;
